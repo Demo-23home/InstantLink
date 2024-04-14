bb7c1fdda48847c4d7894ab03be3e8b122730b6a
demo-23home <zeyadslama23@gmail.com>
2024-04-14 17:42:43 +0200

    Listing Dummy Users on Search Screen

--- app/src/screens/Search.jsx
+++ app/src/screens/Search.jsx
@@ -1,11 +1,143 @@
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import React, { useState } from "react";
-import { SafeAreaView, TextInput, View } from "react-native";
+import { FlatList, SafeAreaView, TextInput, View, Text, TouchableOpacity } from "react-native";
 import Empty from "../common/Empty";
+import Thumbnail from "../common/Thumbnail";
+
+function SearchButton({ user }) {
+  // Add tick if the user is already connected
+  if (user.status === "connected") {
+    return (
+      <FontAwesomeIcon
+        icon="circle-check"
+        size={30}
+        color="#20d080"
+        style={{
+          marginRight: 10,
+        }}
+      />
+    );
+  }
+
+  const data = {};
+
+  switch (user.status) {
+    case "no-connection":
+      data.text = "Connect";
+      data.disabled = false;
+      data.onPress = () => {};
+      break;
+    case "pending-them":
+      data.text = "Pending";
+      data.disabled = true;
+      data.onPress = () => {};
+      break;
+    case "pending-me":
+      data.text = "Accept";
+      data.disabled = false;
+      data.onPress = () => {};
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <TouchableOpacity
+    style={{
+      backgroundColor: data.disabled ? '#505055' : '#202020',
+      paddingHorizontal: 14,
+      height:36,
+      alignItems:'center',
+      justifyContent:'center',
+      borderRadius:18
+    }}
+    disabled = {data.disabled}
+    onPress={data.onPress}
+    >
+    <Text
+    style={{
+      color: data.disabled ? "#808080" : 'white',
+      fontWeight:'bold'
+    }}
+    >
+    {data.text}
+    </Text>
+
+    </TouchableOpacity>
+  )
+
+}
+
+function SearchRow({ user }) {
+  return (
+    <View
+      style={{
+        paddingHorizontal: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderColor: "#f0f0f0",
+        height: 106,
+      }}
+    >
+      <Thumbnail path={user.thumbnail} size={76} />
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 16,
+        }}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "#202020",
+            marginBottom: 4,
+          }}
+        >
+          {user.name}
+        </Text>
+        <Text
+          style={{
+            color: "#606060",
+          }}
+        >
+          {user.username}
+        </Text>
+      </View>
+      <SearchButton user={user} />
+    </View>
+  );
+}
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("");
-  const searchList = null;
+  const searchList = [
+    {
+      thumbnail: null,
+      name: "Silly Name",
+      username: "sillyn",
+      status: "pending-them",
+    },
+    {
+      thumbnail: null,
+      name: "Silly Somthing",
+      username: "sillym",
+      status: "pending-me",
+    },
+    {
+      thumbnail: null,
+      name: "Silly Red",
+      username: "sillyc",
+      status: "connected",
+    },
+    {
+      thumbnail: null,
+      name: "Silly Blue",
+      username: "sillynb",
+      status: "no-connection",
+    },
+  ];
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View
@@ -55,7 +187,12 @@ const SearchScreen = () => {
           centered={false}
         />
       ) : (
-        <View />
+        <FlatList
+          data={searchList}
+          renderItem={({ item }) => (
+            <SearchRow user={item} keyExtractor={(key) => item.username} />
+          )}
+        />
       )}
     </SafeAreaView>
   );
