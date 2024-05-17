c4c35dac4239fe63a1ba6d1b7e9c6b1d8c57f4ed
demo-23home <zeyadslama23@gmail.com>
2024-05-17 16:47:17 +0300

    Updating the Message Screen's Header with the Friend's (Thumbnail, Name)

--- app/src/screens/Messages.jsx
+++ app/src/screens/Messages.jsx
@@ -1,10 +1,47 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { SafeAreaView, View } from "react-native";
 import { Text } from "react-native";
-const MessagesScreen = () => {
+import Thumbnail from "../common/Thumbnail";
+
+function MessageHeader({ friend }) {
   return (
-    <SafeAreaView>
-      <Text>MessagesScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <Thumbnail url={friend.thumbnail} size={30} />
+      <Text
+        style={{
+          color: "#202020",
+          marginLeft: 10,
+          fontSize: 18,
+          fontWeight: "bold",
+        }}
+      >
+        {friend.name}
+      </Text>
+    </View>
+  );
+}
+
+const MessagesScreen = ({ navigation, route }) => {
+  // Update the header
+  const friend = route.params.friend;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => (
+        <MessageHeader friend={friend}/>
+      ) ,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>meassage</Text>
     </SafeAreaView>
   );
 };
